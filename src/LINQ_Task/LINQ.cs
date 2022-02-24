a0a536a4dd545029d8d5ea7f3e9f6f11ad5ae974
sabahb3 <sabahbaara4@gmail.com>
2022-02-24 15:38:57 +0200

    Using LINQ method syntax to check if the second sequence equals the squares of the elements in the first one

---
+++ src/LINQ_Task/LINQ.cs
@@ -0,0 +1,11 @@
+namespace LINQ_Task
+{
+    public class LINQ
+    {
+        public static bool TestForSquares(IEnumerable<int> numbers, IEnumerable<int> squares)
+        {
+            return numbers.Select(number => number * number).SequenceEqual(squares);
+            
+        }
+    }
+}
